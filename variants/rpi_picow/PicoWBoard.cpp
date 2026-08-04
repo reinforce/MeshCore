8cb0a2f649a3ac6232adc64293dc8d05a7f901fe
Aleksei Mamlin <mamlinav@gmail.com>
2026-08-04 09:08:59 +0300

    rpi picow: Fix i2c pins assignment
    Arduino-Pico SDK uses setSDA and setSCL for i2c pins assignment instead of setPins
    
    Signed-off-by: Aleksei Mamlin <mamlinav@gmail.com>

--- variants/rpi_picow/PicoWBoard.cpp
+++ variants/rpi_picow/PicoWBoard.cpp
@@ -27,7 +27,8 @@ void PicoWBoard::begin() {
 #endif
 
 #if defined(PIN_BOARD_SDA) && defined(PIN_BOARD_SCL)
-  Wire.setPins(PIN_BOARD_SDA, PIN_BOARD_SCL);
+  Wire.setSDA(PIN_BOARD_SDA);
+  Wire.setSCL(PIN_BOARD_SCL);
 #endif
 
   Wire.begin();
