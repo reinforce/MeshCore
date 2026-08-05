443039c8c3d534da525920f127d56a595b615921
Aleksei Mamlin <mamlinav@gmail.com>
2026-08-05 12:09:37 +0300

    helpers: Add ability to enable/disable generic vibration

--- src/helpers/ui/GenericVibration.cpp
+++ src/helpers/ui/GenericVibration.cpp
@@ -5,9 +5,11 @@ void GenericVibration::begin() {
   pinMode(PIN_VIBRATION, OUTPUT);
   digitalWrite(PIN_VIBRATION, LOW);
   duration = 0;
+  quiet(false);
 }
 
 void GenericVibration::trigger() {
+  if (_is_quiet) return;
   duration = millis();
   digitalWrite(PIN_VIBRATION, HIGH);
 }
@@ -33,6 +35,14 @@ bool GenericVibration::isVibrating() {
 void GenericVibration::stop() {
   duration = 0;
   digitalWrite(PIN_VIBRATION, LOW);
+}
+
+void GenericVibration::quiet(bool vibra_state) {
+    _is_quiet = vibra_state;
+}
+
+bool GenericVibration::isQuiet() {
+    return _is_quiet;
 }
 
 #endif // ifdef PIN_VIBRATION
