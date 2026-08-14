76da3aee57edbdeec976f27ee09bcee5fa4339fb
Aleksei Mamlin <mamlinav@gmail.com>
2026-08-14 15:13:53 +0300

    ui-new: Add buzzer on/off page with icons
    Signed-off-by: Aleksei Mamlin <mamlinav@gmail.com>

--- examples/companion_radio/ui-new/UITask.cpp
+++ examples/companion_radio/ui-new/UITask.cpp
@@ -93,6 +93,9 @@ class HomeScreen : public UIScreen {
     RADIO,
     BLUETOOTH,
     ADVERT,
+#ifdef PIN_BUZZER
+    BUZZER,
+#endif
 #ifdef PIN_VIBRATION
     VIBRATION,
 #endif
@@ -356,6 +359,16 @@ public:
       display.drawXbm((display.width() - 32) / 2, 18, advert_icon, 32, 32);
       display.setColor(UIColor::secondary_txt);
       display.drawTextCentered(display.width() / 2, 64 - 11, "advert: " PRESS_LABEL);
+#ifdef PIN_BUZZER
+    } else if (_page == HomePage::BUZZER) {
+      display.setColor(UIColor::corp_blue);
+      display.drawXbm((display.width() - 32) / 2, 18,
+          _task->isBuzzerQuiet() ? buzzer_off : buzzer_on,
+          32, 32);
+      display.setColor(UIColor::secondary_txt);
+      display.setTextSize(1);
+      display.drawTextCentered(display.width() / 2, 64 - 11, "toggle: " PRESS_LABEL);
+#endif
 #ifdef PIN_VIBRATION
     } else if (_page == HomePage::VIBRATION) {
       display.setColor(UIColor::corp_blue);
@@ -596,6 +609,12 @@ public:
     }
     if (c == KEY_SELECT && _page == HomePage::DISCOVERY) {
       discovery_disp_names = !discovery_disp_names;
+      return true;
+    }
+#endif
+#ifdef PIN_BUZZER
+    if (c == KEY_ENTER && _page == HomePage::BUZZER) {
+      _task->toggleBuzzer();
       return true;
     }
 #endif
