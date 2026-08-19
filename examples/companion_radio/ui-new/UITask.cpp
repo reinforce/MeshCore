58d6a9bb32711379e85c3ce7c1cd9b56b4a80ecb
Aleksei Mamlin <mamlinav@gmail.com>
2026-08-19 19:11:24 +0300

    ui-new: Increase the message preview size when DISPLAY_UTF8_FONTS is set
    Signed-off-by: Aleksei Mamlin <mamlinav@gmail.com>

--- examples/companion_radio/ui-new/UITask.cpp
+++ examples/companion_radio/ui-new/UITask.cpp
@@ -586,7 +586,11 @@ public:
 };
 
 #ifndef UI_MSG_PREVIEW_SIZE
-  #define UI_MSG_PREVIEW_SIZE 78
+  #ifdef DISPLAY_UTF8_FONTS
+    #define UI_MSG_PREVIEW_SIZE (MAX_TEXT_LEN + 1)
+  #else
+    #define UI_MSG_PREVIEW_SIZE 78
+  #endif
 #endif
 
 class MsgPreviewScreen : public UIScreen {
